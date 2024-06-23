2a26e68345d717e953f4f074165aff0aca13a1c6
Cham <anatol30@list.ru>
2024-06-23 22:52:56 +0300

    0.0.1

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, redirect
 import psycopg2
-import os
+
 
 app = Flask(__name__)
 FLASK_APP = app
 
-def get_db_connection(db_name, user_name, passwd):
+
+def get_db_connection():
+    db_name = 'flaskdb'
+    user_name = "cham_user"
+    passwd = '12345'
     connection = psycopg2.connect(host='localhost',
                                   database=db_name,
                                   user=user_name,
@@ -15,13 +19,35 @@ def get_db_connection(db_name, user_name, passwd):
 
 @app.route('/')
 def index():
-    db_name = 'flaskdb'
-    user_name = "cham_user"
-    passwd = '12345'
-    connection = get_db_connection(db_name, user_name, passwd)
+    connection = get_db_connection()
     cursor = connection.cursor()
     cursor.execute('select * from Foods;')
     foods = cursor.fetchall()
     cursor.close()
     connection.close()
     return render_template('index.html', foods=foods)
+
+
+# GET - переход на страницу  POST - кнопка добавления позиции
+@app.route('/create/', methods=('GET', 'POST'))
+def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        provider = request.form['provider']
+        description = request.form['description']
+
+        connection = get_db_connection()
+        cursor = connection.cursor()
+        cursor.execute('insert into Foods (title, provider, description)'
+                       'values (%s, %s, %s)',
+                       (title, provider, description))
+        connection.commit()
+        cursor.close()
+        connection.close()
+        return redirect(url_for('index'))
+    return render_template('create.html')
+
+
+@app.route('/basket/')
+def basket():
+    return render_template('basket.html')
--- init_db.py
+++ init_db.py
@@ -14,7 +14,7 @@ connection = psycopg2.connect(host='localhost',
 # курсор для взаимодействия с БД
 cursor = connection.cursor()
 
-# создание таблицы
+# создание таблицы товаров
 cursor.execute('DROP TABLE IF EXISTS Foods;')
 cursor.execute('create table Foods (id serial primary key,'
                'title varchar (150) not null,'
@@ -22,6 +22,13 @@ cursor.execute('create table Foods (id serial primary key,'
                'description text,'
                'date_added date default current_timestamp);')
 
+
+# создание таблицы корзины пользователя
+cursor.execute('create table Basket (id serial primary key,'
+               'title varchar (150) not null,'
+               'provider varchar (150) not null,'
+               'description text,'
+               'date_added date default current_timestamp);')
 # вставка строки
 
 cursor.execute('insert into Foods (title, provider, description)'
